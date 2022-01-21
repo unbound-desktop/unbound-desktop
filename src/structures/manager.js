e100e43b8f4727a90eb77bb3f2918f4fa59af8de
slow <73202350+slow@users.noreply.github.com>
2022-01-21 13:53:17 +0000

    Manager imporvements

--- src/structures/manager.js
+++ src/structures/manager.js
@@ -90,6 +90,8 @@ module.exports = class Manager extends Emitter {
          this.entities.set(id, res);
          this.emit('load', id, res);
 
+         if (true/*enabled*/) this.start(id);
+
          return res;
       } catch (e) {
          this.logger.error(`Failed to start ${id}`, e);
@@ -102,16 +104,26 @@ module.exports = class Manager extends Emitter {
       if (!entity) entity = this.load(id);
 
       if (entity && entity.instance && !entity.started) {
-         entity.instance.start?.();
-         entity.started = true;
+         try {
+            entity.instance.start?.();
+            entity.started = true;
+            this.logger.log(`${entity.data.name} was started.`);
+         } catch(e) {
+            this.logger.error(`Coudln't start ${entity.data.name}`, e)
+         }
       }
    }
 
    stop(id) {
       const entity = this.entities.get(id);
       if (entity && entity.instance && entity.started) {
-         entity.instance.stop?.();
-         entity.started = false;
+         try {
+            entity.instance.stop?.();
+            entity.started = false;
+            this.logger.log(`${entity.data.name} was stopped.`);
+         } catch(e) {
+            this.logger.error(`Couldn't stop ${entity.data.name}`, e)
+         }
       }
    }
 
@@ -119,13 +131,31 @@ module.exports = class Manager extends Emitter {
       const entity = this.entities.get(id);
       if (entity) {
          try {
-            entity?.instance?.stop?.();
+            entity.instance?.stop?.();
+            this.logger.log(`${entity.data.name} was stopped & unloaded.`);
          } catch (e) {
-            this.logger.error(`FATAL: ${id} was not able to unload properly, a reload using CTRL+R is recommended.`);
+            this.logger.error(`FATAL: ${id} was not able to unload properly, a reload using CTRL+R is recommended.`, e);
          }
 
          this.entities.delete(id);
       }
+   }
+
+   reload(id) {
+      let entity = this.entities.get(id);
+      
+      try {
+         if (!entity) {
+            return this.load(id);
+         }
+
+         this.unload(id);
+         this.load(id);
+      } catch(e) {
+         throw new Error(`Couldn't reload ${id}`, e);
+      }
+
+      return entity;
    }
 
    fetch() {
