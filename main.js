2105c6de9024987b4f8c69ee27b3a6cf79f0534c
slow <73202350+slow@users.noreply.github.com>
2022-01-21 05:58:23 +0000

    Improve performance in many aspects

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 const Patcher = require('#kernel/core/patchers/BrowserWindowPatcher');
-const { ipcMain } = require('electron');
+const electron = require('electron');
+
+const orig = electron.app.commandLine.appendSwitch;
+
+electron.app.commandLine.appendSwitch = (...args) => {
+   const [key, value] = args;
+   if (key == 'disable-features') {
+      args[1] = `${value},HardwareMediaKeyHandling,HardMediaSessionService,Vulkan`;
+   } else if (key == 'enable-features') {
+      args[1] = `${value},BackForwardCache:TimeToLiveInBackForwardCacheInSeconds/300/should_ignore_blocklists/true/enable_same_site/true,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation`;
+   }
+
+   return orig(...args);
+};
 
 Patcher.patch('unbound', (_, [options]) => {
    options.webPreferences.contextIsolation = false;
    options.webPreferences.nodeIntegration = true;
 });
 
-ipcMain.on('UNBOUND_GET_WINDOW_DATA', (e) => e.returnValue = e.sender.kernelWindowData);
+electron.ipcMain.on('UNBOUND_GET_WINDOW_DATA', (e) => e.returnValue = e.sender.kernelWindowData);
