4ebb14daf8eb7fa9d76732dd8c0a86bfb75a353a
eternal404 <98427312+eternal404@users.noreply.github.com>
2022-01-29 16:40:46 +0000

    Compiler cleanup, remove coffescript

--- src/compilers/jsx.js
+++ src/compilers/jsx.js
@@ -6,9 +6,10 @@ Module._extensions['.jsx'] = (mdl, filename) => {
    const content = readFileSync(filename, 'utf-8');
 
    const { code } = sucrase.transform(content, {
-      transforms: ['jsx', 'flow'],
+      transforms: ['jsx'],
       filePath: filename
    });
+
 
    mdl._compile(code, filename);
 
