75ff4b6061a10d9a63d06db9d8c5b58a5408d8f4
eternal404 <eternal.git@gmail.com>
2022-02-07 13:04:32 +0000

    Tweak AddonCard to handle more bd stuff

--- src/core/components/AddonCard.jsx
+++ src/core/components/AddonCard.jsx
@@ -7,26 +7,27 @@ module.exports = class extends React.PureComponent {
    constructor(props) {
       super(props);
 
-      this.type = this.props.entity.type;
-      this.listener = null;
+      this.client = this.props.entity.type;
    }
 
    renderType(props) {
-      props.className = 'unbound-addon-type-icon';
+      props.className ??= 'unbound-addon-type-icon';
+      props.width ??= 16;
+      props.height ??= 16;
 
-      switch (this.type.toLowerCase()) {
+      switch (this.client.toLowerCase()) {
          case 'betterdiscord':
-            return <Bd width={16} height={16} {...props} />;
+            return <Bd {...props} />;
          case 'powercord':
-            return <Plug width={16} height={16} {...props} />;
+            return <Plug {...props} />;
          default:
             return null;
       }
    }
 
-   get isEnabled() {
+   isEnabled() {
       let type;
-      switch (this.type.toLowerCase()) {
+      switch (this.client.toLowerCase()) {
          case 'powercord':
             type = this.props.type == 'plugins' ? 'pluginManager' : 'styleManager';
             return powercord[type].isEnabled(this.props.entity.entityID);
@@ -41,7 +42,7 @@ module.exports = class extends React.PureComponent {
 
    toggle() {
       let type;
-      switch (this.type.toLowerCase()) {
+      switch (this.client.toLowerCase()) {
          case 'powercord':
             type = this.props.type == 'plugins' ? 'pluginManager' : 'styleManager';
             powercord[type].toggle(this.props.entity.entityID);
@@ -52,13 +53,11 @@ module.exports = class extends React.PureComponent {
             type = this.props.type == 'plugins' ? 'plugins' : 'themes';
             unbound.managers[type].toggle(this.props.entity.id);
       }
-
-      this.forceUpdate();
    }
 
    componentWillMount() {
       let type;
-      switch (this.type.toLowerCase()) {
+      switch (this.client.toLowerCase()) {
          case 'powercord':
             type = this.props.type == 'plugins' ? 'pluginManager' : 'styleManager';
             powercord[type].on('toggle', this.onToggle.bind(this));
@@ -71,7 +70,7 @@ module.exports = class extends React.PureComponent {
    }
 
    getType() {
-      switch (this.type.toLowerCase()) {
+      switch (this.client.toLowerCase()) {
          case 'powercord':
             return this.props.type == 'plugins' ? 'pluginManager' : 'styleManager';
          case 'betterdiscord':
@@ -93,13 +92,14 @@ module.exports = class extends React.PureComponent {
    }
 
    componentWillUnmount() {
-      switch (this.type.toLowerCase()) {
+      let type;
+      switch (this.client.toLowerCase()) {
          case 'powercord':
             type = this.props.type == 'plugins' ? 'pluginManager' : 'styleManager';
             powercord[type].off('toggle', this.onToggle.bind(this));
          case 'betterdiscord':
             type = this.props.type == 'plugins' ? 'Plugins' : 'Themes';
-            BdApi[type].off('toggle', this.onToggle.bind(this));
+            BdApi[type].off('toggled', this.onToggle.bind(this));
          case 'unbound':
             unbound.managers[this.props.type].off('toggle', this.onToggle.bind(this));
       }
@@ -107,8 +107,11 @@ module.exports = class extends React.PureComponent {
 
    onToggle(name) {
       const { entity } = this.props;
-      if (![entity.id, entity.entityID, entity.name].includes(name)) return;
+      if (![entity.id, entity.entityID, entity.name].includes(name)) {
+         return;
+      }
 
+      console.log(name);
       this.forceUpdate();
    }
 
@@ -118,18 +121,18 @@ module.exports = class extends React.PureComponent {
       return (
          <div
             className='unbound-addon-card'
-            style={{ '--entity-color': entity.color ?? entity.instance.color ?? '#3a71c1' }}
+            style={{ '--entity-color': entity?.color ?? entity?.instance?.color ?? '#3a71c1' }}
          >
             <div className='unbound-addon-header'>
                <Text className='unbound-addon-name' size={Text.Sizes.SIZE_16}>
                   {entity.name ?? entity.data?.name ?? entity.displayName}
                </Text>
-               <RelativeTooltip text={`${capitalize(this.type)} Addon`} hideOnClick={false}>
+               <RelativeTooltip text={`${capitalize(this.client)} Addon`} hideOnClick={false}>
                   {p => this.renderType({ ...p })}
                </RelativeTooltip>
                <div className='unbound-addon-controls'>
                   <Switch
-                     checked={this.isEnabled}
+                     checked={this.isEnabled()}
                      onChange={(v) => this.toggle(v)}
                      className='unbound-addon-switch'
                   />
