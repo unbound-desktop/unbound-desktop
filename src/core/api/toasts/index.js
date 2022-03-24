26f370c3e12c0d5076604ad01a33576fd9bc5e47
eternal <eternal.git@gmail.com>
2022-03-24 18:23:58 +0000

    lay out foundation for toasts

---
+++ src/core/api/toasts/index.js
@@ -0,0 +1,47 @@
+const API = require('@structures/api');
+
+const { bindAll, createStore, uuid } = require('@utilities');
+const { create } = require('@patcher');
+
+const toasts = createStore();
+const Patcher = create('unbound-toasts');
+
+module.exports = new class Toasts extends API {
+   constructor() {
+      super();
+
+      this.promises = {
+         cancelled: false,
+         cancel: () => this.promises.cancelled = true
+      };
+
+      bindAll(this, ['send', 'close']);
+   }
+
+   start() {
+
+   };
+
+   send(options) {
+      options.id ??= uuid(5);
+
+      if (toasts.get(options.id)) {
+         options.id = uuid(5);
+      }
+
+      toasts.set(options.id, options);
+
+      return options.id;
+   }
+
+   close(id) {
+      if (toasts.get(id)) {
+         toasts.delete(id);
+      }
+   }
+
+   stop() {
+      this.promises.cancel();
+      Patcher.unpatchAll();
+   }
+};
