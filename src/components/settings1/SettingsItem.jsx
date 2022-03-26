e348c3041001de135703fbdfd9e0e93a47f63d0f
eternal <eternal.git@gmail.com>
2022-03-26 19:43:53 +0000

    why wont this push

---
+++ src/components/settings1/SettingsItem.jsx
@@ -0,0 +1,38 @@
+const { Flex, FormItem, FormText, Divider } = require('@components');
+const { bulk, filters } = require('@webpack');
+const { React } = require('@webpack/common');
+
+const [
+   { marginTop8, marginBottom20 } = {},
+   { description } = {}
+] = bulk(
+   filters.byProps('marginSmall'),
+   filters.byProps('formText', 'description')
+);
+
+module.exports = class SettingsItem extends React.PureComponent {
+   render() {
+      const hasMargin = this.props.hasMargin && marginTop8;
+      return (
+         <FormItem
+            title={this.props.title}
+            required={this.props.required}
+            className={[
+               Flex.Direction.VERTICAL,
+               Flex.Justify.START,
+               Flex.Align.STRETCH,
+               Flex.Wrap.NO_WRAP,
+               marginBottom20
+            ].join(' ')}
+         >
+            {this.props.children}
+            {this.props.description && (
+               <FormText className={[description, hasMargin].filter(Boolean).join(' ')}>
+                  {this.props.description}
+               </FormText>
+            )}
+            <Divider />
+         </FormItem >
+      );
+   }
+};
---
+++ src/components/settings1/TextInput.jsx
@@ -0,0 +1,24 @@
+const { TextInput: Input } = require('@components');
+const { React } = require('@webpack/common');
+
+const SettingsItem = require('./SettingsItem');
+
+module.exports = class TextInput extends React.PureComponent {
+   render() {
+      const { title, description, required } = this.props;
+      const children = this.props.children;
+      delete this.props.children;
+
+      return (
+         <SettingsItem
+            title={title}
+            description={description}
+            required={required}
+            hasMargin={true}
+         >
+            <Input {...this.props} />
+            {children}
+         </SettingsItem>
+      );
+   }
+};
