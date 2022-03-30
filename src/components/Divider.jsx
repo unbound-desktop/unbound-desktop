c473d18946af2ca523012a208237732461ce19ec
eternal <eternal.git@gmail.com>
2022-03-30 16:17:07 +0100

    shorten

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -3,21 +3,21 @@ const { React } = require('@webpack/common');
 
 module.exports = class Divider extends React.Component {
    render() {
-      const direction = this.props.direction;
-      if (!direction || direction === Divider.Directions.VERTICAL) {
-         return <FormDivider {...this.props} />;
+      const { direction } = this.props;
+      if (direction === Divider.Directions.HORIZONTAL) {
+         return <div
+            style={{
+               height: this.props.height ?? '24px',
+               width: this.props.width ?? '1px',
+               margin: this.props.margin ?? '0 8px',
+               flex: '0 0 auto',
+               background: this.props.background ?? 'var(--background-modifier-accent)'
+            }}
+            {...this.props}
+         />;
       }
 
-      return <div
-         style={{
-            height: this.props.height ?? '24px',
-            width: this.props.width ?? '1px',
-            margin: this.props.margin ?? '0 8px',
-            flex: '0 0 auto',
-            background: this.props.background ?? 'var(--background-modifier-accent)'
-         }}
-         {...this.props}
-      />;
+      return <FormDivider {...this.props} />;
    }
 
    static Directions = {
