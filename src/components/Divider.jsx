db6c7ff188ad0af0f58721e857a6bff49ad4cbcf
eternal <eternal.git@gmail.com>
2022-03-30 16:13:18 +0100

    swap divider directions

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -3,20 +3,21 @@ const { React } = require('@webpack/common');
 
 module.exports = class Divider extends React.Component {
    render() {
-      if (this.props.direction === Divider.Directions.VERTICAL) {
-         return <div
-            style={{
-               height: this.props.height ?? '24px',
-               width: this.props.width ?? '1px',
-               margin: this.props.margin ?? '0 8px',
-               flex: '0 0 auto',
-               background: this.props.background ?? 'var(--background-modifier-accent)'
-            }}
-            {...this.props}
-         />;
+      const direction = this.props.direction;
+      if (!direction || direction === Divider.Directions.VERTICAL) {
+         return <FormDivider {...this.props} />;
       }
 
-      return <FormDivider {...this.props} />;
+      return <div
+         style={{
+            height: this.props.height ?? '24px',
+            width: this.props.width ?? '1px',
+            margin: this.props.margin ?? '0 8px',
+            flex: '0 0 auto',
+            background: this.props.background ?? 'var(--background-modifier-accent)'
+         }}
+         {...this.props}
+      />;
    }
 
    static Directions = {
