625b558a92cabfa6956b66625ec6bd824f8adc3c
eternal <eternal.git@gmail.com>
2022-03-31 22:49:10 +0100

    add missing jsdoc param

--- src/modules/utilities/getNestedProp.js
+++ src/modules/utilities/getNestedProp.js
@@ -2,6 +2,7 @@
  * @name getNestedProp
  * @description Gets a nested prop from an object safely by returning null if nothing is found
  * @param {object} object - The object to get the nested prop from
+ * @param {string} path - The path to the nested prop
  * @return {any} Returns null if no prop is found or the prop if it's found.
  */
 
