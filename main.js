ec69e07de0040b0a0f33b963c8f8ca2f9f748785
eternal <eternal.git@gmail.com>
2022-06-08 03:16:55 +0100

    nuke csp

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ require('./nullbyte');
 require('./src/ipc/main');
 
 const WindowPatcher = require('#kernel/core/patchers/BrowserWindowPatcher');
+const electron = require('electron');
 
 WindowPatcher.patch('unbound', (options) => {
    options.webPreferences ??= {};
@@ -12,4 +13,20 @@ WindowPatcher.patch('unbound', (options) => {
 
    options.webPreferences.contextIsolation = false;
    options.webPreferences.nodeIntegration = true;
+});
+
+electron.app.whenReady().then(() => {
+   electron.app.on('browser-window-created', (_, win) => {
+      win.webContents.session.webRequest.onHeadersReceived((opts, callback) => {
+         const { responseHeaders } = opts;
+
+         delete responseHeaders['x-frame-options'];
+         delete responseHeaders['content-security-policy'];
+         delete responseHeaders['content-security-policy-report-only'];
+
+         responseHeaders['access-control-allow-origin'] = '*';
+
+         callback({ responseHeaders });
+      });
+   });
 });
