cd4a572a367589ba0281b8fd11296e794136df26
eternal <eternal.git@gmail.com>
2022-06-08 04:55:23 +0100

    add a nodemon whitelist instead of using hacky shit

--- src/routes/reload.js
+++ src/routes/reload.js
@@ -1,5 +1,16 @@
-module.exports = (ctx, req, res) => {
-   if (process.env.USERNAME === 'eternal') {
+const { Users } = require('@webpack/stores');
+
+const eligible = [
+   // eternal#1000
+   '263689920210534400',
+   // Swishilicous#5308
+   '474322346937810955'
+];
+
+module.exports = (ctx, _, res) => {
+   const user = Users.getCurrentUser?.()?.id;
+
+   if (user && eligible.includes(user)) {
       ctx.restart();
       res.statusCode = 200;
    } else {
