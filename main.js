8fe5a5ec1ee7d9d41e02c78e8ee240a81dc97694
eternal <eternal.git@gmail.com>
2022-06-13 04:49:23 +0100

    only modify access control allow origin if credential mode isn't set to true, this should fix any issues with stripe or anything that relies on cross-site cookies

--- main.js
+++ main.js
@@ -15,16 +15,57 @@ WindowPatcher.patch('unbound', (options) => {
    options.webPreferences.nodeIntegration = true;
 });
 
+const headers = {
+   delete: [
+      'x-frame-options',
+      'content-security-policy',
+      'content-security-policy-report-only'
+   ],
+   append: [
+      {
+         name: 'access-control-allow-origin',
+         value: '*',
+         exists: {
+            name: 'access-control-allow-credentials',
+            condition: false
+         }
+      }
+   ]
+}
+
 electron.app.whenReady().then(() => {
    electron.app.on('browser-window-created', (_, win) => {
       win.webContents.session.webRequest.onHeadersReceived((opts, callback) => {
          const { responseHeaders } = opts;
+         const orig = Object.keys(responseHeaders);
 
-         delete responseHeaders['x-frame-options'];
-         delete responseHeaders['content-security-policy'];
-         delete responseHeaders['content-security-policy-report-only'];
+         for (let i = 0; i < headers.delete.length; i++) {
+            const header = headers.delete[i];
+            if (!header) continue;
 
-         responseHeaders['access-control-allow-origin'] = '*';
+            const name = orig.find(o => o.toLowerCase() === header.toLowerCase());
+            if (!name) continue;
+
+            delete responseHeaders[name];
+         }
+
+         for (let i = 0; i < headers.append.length; i++) {
+            const data = headers.append[i];
+            if (!data) continue;
+
+            if (data.exists?.name) {
+               const res = orig.find(r => r.toLowerCase() === data.exists.name);
+               
+               if (data.exists.condition !== Boolean(res)) {
+                  continue;
+               }
+            }
+
+            const name = orig.find(o => o.toLowerCase() === data.name.toLowerCase());
+            if (!name) continue;
+
+            responseHeaders[name] = data.value;
+         }
 
          callback({ responseHeaders });
       });
