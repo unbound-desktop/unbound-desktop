3f307195d07a4abfaf3099a39bc0f73ff31ce23b
eternal <eternal.git@gmail.com>
2022-07-30 18:37:53 +0100

    modify how the clyde api works

--- src/client/api/clyde.tsx
+++ src/client/api/clyde.tsx
@@ -4,18 +4,18 @@ import type { MessageJSON } from 'discord-types/general';
 import { Clyde } from '@webpack/common';
 import { IDs } from '@constants';
 
-const Lodash = window._;
-
 export function initialize() {
    void AsyncUsers.getUser(IDs.BOT);
 }
 
-export function send(channel: string | undefined, message: MessageJSON) {
-   if (!channel) channel = SelectedChannels.getChannelId();
+export function send(message: MessageJSON, channel: string = SelectedChannels.getChannelId()) {
+   const payload = {
+      ...Clyde.createBotMessage(channel, message?.content),
+      state: 'SENT',
+      author: Users.getUser(IDs.BOT),
+      channel_id: channel,
+      ...message
+   };
 
-   Messages?.receiveMessage(channel, Lodash.merge({},
-      Clyde.createBotMessage(channel, message?.content),
-      { state: 'SENT', author: Users.getUser(IDs.BOT), channel_id: channel },
-      message
-   ));
+   Messages?.receiveMessage(channel, payload);
 }
