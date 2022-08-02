5e6d9c11f3cb062883f1d95504d3ecc6c70af51b
eternal <eternal.git@gmail.com>
2022-08-02 14:06:44 +0100

    fix experiments

--- src/client/core/builtins/experiments.ts
+++ src/client/core/builtins/experiments.ts
@@ -1,5 +1,6 @@
-import { Constants } from '@webpack/common';
+import { Constants, Dispatcher } from '@webpack/common';
 import { Users } from '@webpack/stores';
+import { sleep } from '@utilities';
 import { create } from '@patcher';
 
 const Patcher = create('unbound-experiments');
@@ -9,10 +10,25 @@ export const data = {
    id: 'dev.experiments',
    default: false,
    restart: true,
-   wait: false
+   wait: false,
+
+   cancelled: false
 };
 
-export function initialize() {
+export async function initialize() {
+   data.cancelled = false;
+
+   // Wait for dispatcher handlers
+   const events = Dispatcher._orderedActionHandlers;
+   while (!events.CONNECTION_OPEN) await sleep(10);
+
+   // Don't run code block incase experiments is toggled before the above resolves
+   if (data.cancelled) return;
+
+   // Grab handlers for CONNECTION_OPEN
+   const Handlers = events['CONNECTION_OPEN'];
+
+   // Spoof the staff flag
    const unpatch = Patcher.after(Users, 'getCurrentUser', (_, __, res) => {
       if (!res) return;
 
@@ -21,7 +37,6 @@ export function initialize() {
             if (prop === 'hasFlag') {
                return function (flag) {
                   if (flag === Constants.UserFlags.STAFF) {
-                     unpatch();
                      return true;
                   }
 
@@ -33,8 +48,27 @@ export function initialize() {
          }
       });
    });
+
+   // Call the dispatcher action handler with the spoofed flags to internally allow bucket overrides
+   const ExperimentStore = Handlers.find(h => h.name === 'ExperimentStore');
+   if (ExperimentStore) ExperimentStore.actionHandler({
+      type: 'CONNECTION_OPEN',
+      guildExperiments: [],
+      experiments: [],
+      user: {
+         ...Users.getCurrentUser(),
+         flags: 1,
+      }
+   });
+
+   // Call the dispatcher action handler to update isDeveloper internally
+   const DeveloperExperimentStore = Handlers.find(h => h.name === 'DeveloperExperimentStore');
+   if (DeveloperExperimentStore) DeveloperExperimentStore.actionHandler();
+
+   unpatch();
 }
 
 export function shutdown() {
+   data.cancelled = true;
    Patcher.unpatchAll();
 }
