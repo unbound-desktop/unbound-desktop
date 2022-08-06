4ecedbb3b5e74ffdd872d8536a19d365b5f085e8
eternal <eternal.git@gmail.com>
2022-08-06 16:57:36 +0100

    experiment builtin tweaks

--- src/client/core/builtins/experiments.ts
+++ src/client/core/builtins/experiments.ts
@@ -19,14 +19,10 @@ export async function initialize() {
    data.cancelled = false;
 
    // Wait for dispatcher handlers
-   const events = Dispatcher._orderedActionHandlers;
-   while (!events.CONNECTION_OPEN) await sleep(10);
+   const events = Object.values(Dispatcher._dependencyGraph.nodes);
 
    // Don't run code block incase experiments is toggled before the above resolves
    if (data.cancelled) return;
-
-   // Grab handlers for CONNECTION_OPEN
-   const Handlers = events['CONNECTION_OPEN'];
 
    // Spoof the staff flag
    const unpatch = Patcher.after(Users, 'getCurrentUser', (_, __, res) => {
@@ -49,23 +45,34 @@ export async function initialize() {
       });
    });
 
-   // Call the dispatcher action handler with the spoofed flags to internally allow bucket overrides
-   const ExperimentStore = Handlers.find(h => h.name === 'ExperimentStore');
-   if (ExperimentStore) ExperimentStore.actionHandler({
-      type: 'CONNECTION_OPEN',
-      guildExperiments: [],
-      experiments: [],
-      user: {
-         ...Users.getCurrentUser(),
-         flags: 1,
-      }
-   });
+   function onDispatch() {
+      // Call the dispatcher action handler with the spoofed flags to internally allow bucket overrides
+      const ExperimentStore = events.find(h => h.name === 'ExperimentStore');
+      console.log(ExperimentStore);
+      if (ExperimentStore) ExperimentStore.actionHandler.CONNECTION_OPEN({
+         type: 'CONNECTION_OPEN',
+         guildExperiments: [],
+         experiments: [],
+         user: {
+            ...Users.getCurrentUser(),
+            flags: 1,
+         }
+      });
 
-   // Call the dispatcher action handler to update isDeveloper internally
-   const DeveloperExperimentStore = Handlers.find(h => h.name === 'DeveloperExperimentStore');
-   if (DeveloperExperimentStore) DeveloperExperimentStore.actionHandler();
+      // Call the dispatcher action handler to update isDeveloper internally
+      const DeveloperExperimentStore = events.find(h => h.name === 'DeveloperExperimentStore');
+      if (DeveloperExperimentStore) DeveloperExperimentStore.actionHandler.CONNECTION_OPEN();
 
-   unpatch();
+      Dispatcher.unsubscribe('CONNECTION_OPEN', onDispatch);
+      unpatch();
+   }
+
+   if (Users.getCurrentUser()) {
+      onDispatch();
+   } else {
+      Dispatcher.subscribe('CONNECTION_OPEN', onDispatch);
+   }
+
 }
 
 export function shutdown() {
