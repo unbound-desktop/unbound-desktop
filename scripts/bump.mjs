976a3f03f27f5a94f599007192cc0fa147b3fe49
eternal <eternal.git@gmail.com>
2022-08-21 21:42:23 +0100

    Update bump.mjs

--- scripts/bump.mjs
+++ scripts/bump.mjs
@@ -1,7 +1,8 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-const manifest = JSON.parse(fs.readFileSync('index.json', 'utf-8'));
+const contents = fs.readFileSync('index.json', 'utf-8');
+const manifest = JSON.parse(contents);
 const tag = execSync('git describe --tags --abbrev=0', { encoding: 'utf-8' });
 
 manifest.version = tag.slice(1).trim();
